b96d45f42d791b78fc7d90a1477e7167b065c906
jjmin321 <jjmin321@naver.com>
2021-03-22 12:06:54 +0900

    initial project

---
+++ src/main/java/jejeongmin/MakeAnything/MakeAnythingApplication.java
@@ -0,0 +1,13 @@
+package jejeongmin.MakeAnything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeAnythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jejeongmin/MakeAnything/MakeAnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package jejeongmin.MakeAnything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeAnythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
