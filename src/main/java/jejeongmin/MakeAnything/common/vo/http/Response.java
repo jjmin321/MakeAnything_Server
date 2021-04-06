130482f07cdeedaa564ec5b446e6315a6346775d
jjmin321 <jjmin321@naver.com>
2021-04-06 12:10:03 +0900

    [ADD] common - Response & ResponseData VO for REST API

--- src/main/java/jejeongmin/MakeAnything/common/vo/http/Response.java
+++ src/main/java/jejeongmin/MakeAnything/common/vo/http/Response.java
@@ -4,20 +4,13 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class RealResponse {
+public class Response {
     private final int status;
     private final String message;
-    private Object data;
 
-    public RealResponse(HttpStatus status, String message) {
+    public Response(HttpStatus status, String message) {
         this.status = status.value();
         this.message = message;
-    }
-
-    public RealResponse(HttpStatus status, String message, Object data) {
-        this.status = status.value();
-        this.message = message;
-        this.data = data;
     }
 
 }
--- src/main/java/jejeongmin/MakeAnything/common/vo/http/ResponseData.java
+++ src/main/java/jejeongmin/MakeAnything/common/vo/http/ResponseData.java
@@ -1,4 +1,14 @@
 package jejeongmin.MakeAnything.common.vo.http;
 
-public class ResponseData {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ResponseData extends Response{
+    private final Object data;
+
+    public ResponseData(HttpStatus status, String message, Object data) {
+        super(status, message);
+        this.data = data;
+    }
 }
