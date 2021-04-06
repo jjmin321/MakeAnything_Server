f92781743f652d8a5fe5a379fbc85d35ea286b4d
jjmin321 <jjmin321@naver.com>
2021-04-06 12:28:51 +0900

    [ADD] user - UserDto for SignIn API

--- src/main/java/jejeongmin/MakeAnything/user/domain/dto/UserDto.java
+++ src/main/java/jejeongmin/MakeAnything/user/domain/dto/UserDto.java
@@ -1,4 +1,14 @@
 package jejeongmin.MakeAnything.user.domain.dto;
 
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
 public class UserDto {
+    @NotBlank
+    private String id;
+
+    @NotBlank
+    private String pw;
 }
