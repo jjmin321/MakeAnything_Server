17751eca2f8216ece383900814e724ca8fb69625
jjmin321 <jjmin321@naver.com>
2021-04-08 13:33:29 +0900

    [UPDATE] .gitignore

--- src/main/java/jejeongmin/MakeAnything/user/service/UserServiceImpl.java
+++ src/main/java/jejeongmin/MakeAnything/user/service/UserServiceImpl.java
@@ -4,13 +4,14 @@ import jejeongmin.MakeAnything.common.lib.Encrypt;
 import jejeongmin.MakeAnything.user.domain.dto.UserDto;
 import jejeongmin.MakeAnything.user.domain.entity.User;
 import jejeongmin.MakeAnything.user.domain.repository.UserRepository;
-import jejeongmin.MakeAnything.user.retrofit.service.DodamService;
-import jejeongmin.MakeAnything.user.retrofit.vo.DodamUserDataVo;
-import jejeongmin.MakeAnything.user.retrofit.vo.DodamUserVo;
+import jejeongmin.MakeAnything.common.retrofit.service.DodamService;
+import jejeongmin.MakeAnything.common.retrofit.vo.DodamUserDataVo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -37,6 +38,7 @@ public class UserServiceImpl implements UserService {
             DodamUserDataVo dodamUserDataVo = dodamService.getUserInfo(dodamToken);
             User mappedUser = modelMapper.map(dodamUserDataVo, User.class);
             User createdUser = userRepository.save(mappedUser);
+            // String jwt = jwt.createToken
             return createdUser;
         } catch (Exception e) {
             throw e;
