76e5966eb44a6d00c9941ad745c8d6707525c61f
jjmin321 <jjmin321@naver.com>
2021-04-21 14:05:42 +0900

    [ADD] AuthorizationCheck AOP - JOINPOINT, POINTCUT, ADVICE

--- src/main/java/jejeongmin/MakeAnything/common/aspect/AuthorizationAspect.java
+++ src/main/java/jejeongmin/MakeAnything/common/aspect/AuthorizationAspect.java
@@ -1,4 +1,26 @@
 package jejeongmin.MakeAnything.common.aspect;
 
+import jejeongmin.MakeAnything.common.exception.AuthorizationException;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Aspect
 public class AuthorizationAspect {
+
+    @Pointcut("@annotation(jejeongmin.MakeAnything.common.annotation.AuthorizationCheck)")
+    public void authorizationCheck() {}
+
+    @Before("authorizationCheck() && args(request)")
+    public boolean isExist(HttpServletRequest request) throws AuthorizationException {
+        if (request.getAttribute("user") == null) {
+            throw new AuthorizationException("유저 정보가 없음");
+        }
+        return true;
+    }
+
 }
