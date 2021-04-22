75e278e01e204186883e94b3275f130459db6681
jjmin321 <jjmin321@naver.com>
2021-04-22 10:43:12 +0900

    [DELETE] try-catch block(RuntimeException)

--- src/main/java/jejeongmin/MakeAnything/common/interceptor/AuthInterceptor.java
+++ src/main/java/jejeongmin/MakeAnything/common/interceptor/AuthInterceptor.java
@@ -4,10 +4,7 @@ import jejeongmin.MakeAnything.common.lib.Jwt;
 import jejeongmin.MakeAnything.user.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.HandlerInterceptor;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,21 +16,10 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        try {
-            String token = jwt.extract(request, "Bearer");
-
-            if (!ObjectUtils.isEmpty(token)) {
-                User user = jwt.validateToken(token);
-                request.setAttribute("user", user);
-            } else {
-                if (request.getMethod().equals("OPTIONS")) {
-                    return true;
-                }
-            }
-            return true;
-        } catch (HttpClientErrorException e) {
-            throw e;
-        }
+        String token = jwt.extract(request, "Bearer");
+        User user = jwt.validateToken(token);
+        request.setAttribute("user", user);
+        return true;
     }
 
 }
