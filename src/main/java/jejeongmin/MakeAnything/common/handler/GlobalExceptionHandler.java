cc61c37a5014e9f3598d91056784d6e45e51a21a
jjmin321 <jjmin321@naver.com>
2021-04-22 10:30:15 +0900

    [ADD] JWT Exception Handler

--- src/main/java/jejeongmin/MakeAnything/common/handler/GlobalExceptionHandler.java
+++ src/main/java/jejeongmin/MakeAnything/common/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package jejeongmin.MakeAnything.common.handler;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 import jejeongmin.MakeAnything.common.exception.AuthorizationException;
 import jejeongmin.MakeAnything.common.exception.EncryptException;
 import jejeongmin.MakeAnything.common.vo.http.Response;
@@ -33,6 +36,26 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AuthorizationException.class)
     public Response handleAuthorizationException(AuthorizationException e) {
         return new ResponseError(HttpStatus.UNAUTHORIZED, e.getMessage(), "AuthorizationException");
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public Response handleExpiredJwtException(ExpiredJwtException e) {
+        return new ResponseError(HttpStatus.FORBIDDEN, "JWT 토큰이 만료되어 요청을 수행할 수 없습니다", "ExpiredJwtException");
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public Response handleSignatureException(SignatureException e) {
+        return new ResponseError(HttpStatus.FORBIDDEN, "JWT 토큰이 위조되어 요청을 수행할 수 없습니다", "SignatureException");
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public Response handleMalformedJwtException(MalformedJwtException e) {
+        return new ResponseError(HttpStatus.FORBIDDEN, "JWT 토큰이 위조되어 요청을 수행할 수 없습니다", "MalformedJwtException");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Response handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseError(HttpStatus.NOT_ACCEPTABLE, "JWT 토큰이 없어 요청을 수행할 수 없습니다", "IllegalArgumentException");
     }
 
 }
