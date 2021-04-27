d06c4da9f2a3de41602e5b4ea2915645cf6c2f19
jjmin321 <jjmin321@naver.com>
2021-04-27 10:00:12 +0900

    [ADD] makeDirectoryIfNotExist method

--- src/main/java/jejeongmin/MakeAnything/common/lib/File.java
+++ src/main/java/jejeongmin/MakeAnything/common/lib/File.java
@@ -1,5 +1,6 @@
 package jejeongmin.MakeAnything.common.lib;
 
+import jejeongmin.MakeAnything.common.exception.MakeDirectoryException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,9 +14,20 @@ import java.util.UUID;
 @Component
 public class File {
 
+    public void makeDirectoryIfNotExist(Path target) {
+        if (!target.getParent().toFile().exists()) {
+            if (target.getParent().toFile().mkdir()) {
+                System.out.println("디렉토리가 생성됨");
+            } else {
+                throw new MakeDirectoryException();
+            }
+        }
+    }
+
     public String uploadFile(MultipartFile file) throws IOException {
         String fileName = StringUtils.cleanPath(UUID.randomUUID().toString() + "-" + file.getOriginalFilename());
         Path target = Paths.get("./src/main/resources/static").toAbsolutePath().normalize().resolve(fileName);
+        makeDirectoryIfNotExist(target);
         Files.copy(file.getInputStream(), target, StandardCopyOption.REPLACE_EXISTING);
         return fileName;
     }
