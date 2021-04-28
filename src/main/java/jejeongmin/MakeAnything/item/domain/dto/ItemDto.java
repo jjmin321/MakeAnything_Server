766743b50a403993d28460d871dd608346d21418
jjmin321 <jjmin321@naver.com>
2021-04-28 17:41:35 +0900

    [ADD] createdAt column in Item entity

--- src/main/java/jejeongmin/MakeAnything/item/domain/dto/ItemDto.java
+++ src/main/java/jejeongmin/MakeAnything/item/domain/dto/ItemDto.java
@@ -1,4 +1,31 @@
 package jejeongmin.MakeAnything.item.domain.dto;
 
+import jejeongmin.MakeAnything.common.enums.ItemType;
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
 public class ItemDto {
+
+    @NotNull
+    private ItemType type;
+
+    @NotBlank
+    private String user;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    private Integer price;
+
+    @NotBlank
+    private String thumbnail;
+
 }
--- src/main/java/jejeongmin/MakeAnything/item/domain/entity/Item.java
+++ src/main/java/jejeongmin/MakeAnything/item/domain/entity/Item.java
@@ -1,12 +1,20 @@
 package jejeongmin.MakeAnything.item.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jejeongmin.MakeAnything.common.enums.ItemType;
 import jejeongmin.MakeAnything.user.domain.entity.User;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity(name = "item")
 @Getter
@@ -15,6 +23,9 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idx;
+
+    @Enumerated(EnumType.ORDINAL)
+    private ItemType type;
 
     @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
     @JoinColumn(columnDefinition = "id")
@@ -32,7 +43,7 @@ public class Item {
     @Column(length = 50, nullable = false)
     private String thumbnail;
 
-    @Enumerated(EnumType.ORDINAL)
-    private ItemType type;
+    @CreationTimestamp
+    private LocalDateTime createdAt; // Date(UTC Issue in java) -> LocalDateTime
 
 }
