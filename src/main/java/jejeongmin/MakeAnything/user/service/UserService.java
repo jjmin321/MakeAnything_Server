c244e429361aa46471be2227575524d170dd7ca8
jjmin321 <jjmin321@naver.com>
2021-05-25 21:59:12 +0900

    [ADD] @Transactional in userService

--- src/main/java/jejeongmin/MakeAnything/user/service/UserService.java
+++ src/main/java/jejeongmin/MakeAnything/user/service/UserService.java
@@ -1,9 +1,13 @@
 package jejeongmin.MakeAnything.user.service;
 
 import jejeongmin.MakeAnything.user.domain.dto.UserDto;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.io.IOException;
 import java.util.Map;
 
+@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 public interface UserService {
 
     Map<String, String> signIn(UserDto userDto) throws IOException;
