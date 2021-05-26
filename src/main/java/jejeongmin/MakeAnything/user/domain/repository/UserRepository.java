d42eb5b3049bf326625d6562537961a451bf8325
jjmin321 <jjmin321@naver.com>
2021-05-26 16:08:09 +0900

    [MODIFY] findById return type Optional<User> -> User

--- src/main/java/jejeongmin/MakeAnything/user/domain/repository/UserRepository.java
+++ src/main/java/jejeongmin/MakeAnything/user/domain/repository/UserRepository.java
@@ -3,9 +3,8 @@ package jejeongmin.MakeAnything.user.domain.repository;
 import jejeongmin.MakeAnything.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findById(String id);
+    User findById(String id);
 }
